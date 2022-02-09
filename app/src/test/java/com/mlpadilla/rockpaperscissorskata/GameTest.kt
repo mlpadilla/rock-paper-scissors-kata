196084612cb3ce129582db11313f7ab6be00ad4a
mlpadilla <10867537+mlpadilla@users.noreply.github.com>
2022-02-09 22:43:41 +0100

    Refactor: Improve coding style for GameTest

--- app/src/test/java/com/mlpadilla/rockpaperscissorskata/GameTest.kt
+++ app/src/test/java/com/mlpadilla/rockpaperscissorskata/GameTest.kt
@@ -1,126 +1,93 @@
 package com.mlpadilla.rockpaperscissorskata
 
 import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.core.spec.style.behaviorSpec
 import io.kotest.matchers.shouldBe
 
 class GameTest: BehaviorSpec({
-    given("a player with gesture 'rock'") {
-        val playerGesture = Gesture.ROCK
-        and("an opponent with gesture 'scissors'") {
-            val opponentGesture = Gesture.SCISSORS
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("the player wins") {
-                    winner shouldBe Winner.PLAYER
-                }
-            }
-        }
-        and("an opponent with gesture 'paper'") {
-            val opponentGesture = Gesture.PAPER
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("the opponent wins") {
-                    winner shouldBe Winner.OPPONENT
-                }
-            }
-        }
-        and("an opponent with gesture 'rock'") {
-            val opponentGesture = Gesture.ROCK
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("it's a draw") {
-                    winner shouldBe Winner.DRAW
-                }
-            }
-        }
-    }
-
-    given("a player with gesture 'scissors'") {
-        val playerGesture = Gesture.SCISSORS
-        and("an opponent with gesture 'rock'") {
-            val opponentGesture = Gesture.ROCK
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("the opponent wins") {
-                    winner shouldBe Winner.OPPONENT
-                }
-            }
-        }
-        and("an opponent with gesture 'paper'") {
-            val opponentGesture = Gesture.PAPER
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("the player wins") {
-                    winner shouldBe Winner.PLAYER
-                }
-            }
-        }
-        and("an opponent with gesture 'scissors'") {
-            val opponentGesture = Gesture.SCISSORS
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("it's a draw") {
-                    winner shouldBe Winner.DRAW
-                }
-            }
-        }
-    }
-
-    given("a player with gesture 'paper'") {
-        val playerGesture = Gesture.PAPER
-        and("an opponent with gesture 'scissors'") {
-            val opponentGesture = Gesture.SCISSORS
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("the opponent wins") {
-                    winner shouldBe Winner.OPPONENT
-                }
-            }
-        }
-        and("an opponent with gesture 'rock'") {
-            val opponentGesture = Gesture.ROCK
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("the player wins") {
-                    winner shouldBe Winner.PLAYER
-                }
-            }
-        }
-        and("an opponent with gesture 'paper'") {
-            val opponentGesture = Gesture.PAPER
-            `when`("playing") {
-                val winner = Game.play(
-                    playerGesture = playerGesture,
-                    opponentGesture = opponentGesture
-                )
-                then("it's a draw") {
-                    winner shouldBe Winner.DRAW
-                }
-            }
-        }
-    }
+    include(
+        testPlay(
+            playerGesture = Gesture.ROCK,
+            opponentGesture = Gesture.SCISSORS,
+            winner = Winner.PLAYER
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.ROCK,
+            opponentGesture = Gesture.PAPER,
+            winner = Winner.OPPONENT
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.ROCK,
+            opponentGesture = Gesture.ROCK,
+            winner = Winner.DRAW
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.SCISSORS,
+            opponentGesture = Gesture.ROCK,
+            winner = Winner.OPPONENT
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.SCISSORS,
+            opponentGesture = Gesture.PAPER,
+            winner = Winner.PLAYER
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.SCISSORS,
+            opponentGesture = Gesture.SCISSORS,
+            winner = Winner.DRAW
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.PAPER,
+            opponentGesture = Gesture.SCISSORS,
+            winner = Winner.OPPONENT
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.PAPER,
+            opponentGesture = Gesture.ROCK,
+            winner = Winner.PLAYER
+        )
+    )
+    include(
+        testPlay(
+            playerGesture = Gesture.PAPER,
+            opponentGesture = Gesture.PAPER,
+            winner = Winner.DRAW
+        )
+    )
 })
+
+fun testPlay(
+    playerGesture: Gesture,
+    opponentGesture: Gesture,
+    winner: Winner
+) = behaviorSpec {
+    given("a player with gesture '${playerGesture.name}' AND an opponent with gesture '${opponentGesture.name}'") {
+        val playerGesture = playerGesture
+
+        val opponentGesture = opponentGesture
+        `when`("playing") {
+            val winner = Game.play(
+                playerGesture = playerGesture,
+                opponentGesture = opponentGesture
+            )
+            then("the winner is: '${winner.name}'") {
+                winner shouldBe winner
+            }
+        }
+
+    }
+}
